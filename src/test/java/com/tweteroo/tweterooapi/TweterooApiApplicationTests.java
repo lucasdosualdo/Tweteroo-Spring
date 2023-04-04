1401d688b4149c26943236c25116a4f2b47d61a3
Your Name <lucasdosualdo@gmail.com>
2023-04-04 20:39:09 -0300

    feat: create spring project

---
+++ src/test/java/com/tweteroo/tweterooapi/TweterooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroo.tweterooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
