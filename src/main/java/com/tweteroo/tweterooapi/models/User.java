48776e869c976c173f2adffbd994382353208293
Your Name <lucasdosualdo@gmail.com>
2023-04-04 20:54:16 -0300

    feat: create user model

---
+++ src/main/java/com/tweteroo/tweterooapi/models/User.java
@@ -0,0 +1,27 @@
+package com.tweteroo.tweterooapi.models;
+
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String username;
+
+    @URL
+    @Column(nullable = false)
+    private String avatar;
+}
